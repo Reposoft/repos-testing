b74c240eb33801f566e9d979b8d4970c69f3d54b
Staffan Olsson <staffan@repos.se>
2013-09-03 15:22:29 +0000

    Realized an issue with how we currently initialize test indexing - custom cores are very difficult to use because they need to be injected into services, one of which is likely the custom handler

---
+++ src/test/java/se/repos/testing/indexing/svn/DummyItemHandler.java
@@ -0,0 +1,44 @@
+package se.repos.testing.indexing.svn;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.management.RuntimeErrorException;
+
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.common.SolrInputDocument;
+
+import se.repos.indexing.item.IndexingItemHandler;
+import se.repos.indexing.item.IndexingItemProgress;
+
+public class DummyItemHandler implements IndexingItemHandler {
+
+	private SolrServer dummycore;
+
+	DummyItemHandler(SolrServer dummycore) {
+		this.dummycore = dummycore;
+	}
+	
+	@Override
+	public void handle(IndexingItemProgress progress) {
+		SolrInputDocument doc = new SolrInputDocument();
+		doc.addField("id", "" + System.currentTimeMillis() + "" + Math.random());
+		try {
+			dummycore.add(doc);
+			dummycore.commit();
+		} catch (SolrServerException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+
+	@Override
+	public Set<Class<? extends IndexingItemHandler>> getDependencies() {
+		return new HashSet<Class<? extends IndexingItemHandler>>();
+	}
+
+}
--- src/test/java/se/repos/testing/indexing/svn/SvnTestIndexingTest.java
+++ src/test/java/se/repos/testing/indexing/svn/SvnTestIndexingTest.java
@@ -28,11 +28,13 @@ import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.wc.SVNRevision;
 import org.tmatesoft.svn.core.wc2.SvnCopySource;
+import org.tmatesoft.svn.core.wc2.SvnImport;
 import org.tmatesoft.svn.core.wc2.SvnOperationFactory;
 import org.tmatesoft.svn.core.wc2.SvnRemoteCopy;
 import org.tmatesoft.svn.core.wc2.SvnRemoteDelete;
 import org.tmatesoft.svn.core.wc2.SvnTarget;
 
+import se.repos.testing.indexing.TestIndexOptions;
 import se.repos.testing.indexing.svn.SvnTestIndexing;
 import se.simonsoft.cms.testing.svn.CmsTestRepository;
 import se.simonsoft.cms.testing.svn.SvnTestSetup;
@@ -229,6 +231,36 @@ public class SvnTestIndexingTest {
 	    assertEquals("Should have got the revision from the hook", 2, revs.get(0).intValue());
 	    assertTrue("The commit should block while the indexing operation runs, got " + timeCommit, timeCommit > 1000);
 	    assertTrue("The actual commont shouldn't take long, got " + timeCommit, timeCommit < 10000);
-	}	
+	}
+	
+	/**
+	 * Catch 22: Tests that use additional cores need the SolrServer instance for dependency injection into services,
+	 * but don't have an instance of SvnTestIndexing until they can create their handler - also dependency injection.
+	 * @throws Exception 
+	 */
+	@Test(timeout=100000)
+	public void testCustomCore() throws SolrServerException, Exception {
+		CmsTestRepository repo = SvnTestSetup.getInstance().getRepository();
+		
+		TestIndexOptions options = new TestIndexOptions().itemDefaults();
+		options.addCore("dummycore", "se/repos/indexing/testing/solr/dummycore/**");
+		SolrServer coreNotCreatedYet = null; // TODO how to get hold of dummycore here?
+		options.addHandler(new DummyItemHandler(coreNotCreatedYet));
+		
+		SvnTestIndexing instance = SvnTestIndexing.getInstance(options);
+		instance.enable(repo);
+		
+		SvnImport im = repo.getSvnkitOp().createImport();
+		File tmp = File.createTempFile("temp-" + this.getClass().getName(), "");
+		im.setSource(tmp);
+		im.setSingleTarget(SvnTarget.fromURL(SVNURL.parseURIEncoded(repo.getUrl() + "/temp")));
+		im.run();
+		tmp.delete();
+		
+		assertEquals("Should have indexed through the promised core and the given handler", 1, 
+				coreNotCreatedYet.query(new SolrQuery("*:*")).getResults().getNumFound());
+		assertEquals("Core by name should be the same", 1,
+				instance.getCore("dummycore").query(new SolrQuery("*:*")).getResults().getNumFound());
+	}
 
 }
