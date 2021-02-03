d65ca86d5908447feb4e43bbc773f7991482d010
Thomas Åkesson <thomas.akesson@simonsoft.se>
2021-02-03 13:20:26 +0100

    Moved indexing lookup implementations from svnlook to http. Need to inject SVNRepository now when no longer injecting Svnlook provider?

--- src/main/java/se/repos/testing/cmstest/SingleRepoSvnkitIndexingModule.java
+++ src/main/java/se/repos/testing/cmstest/SingleRepoSvnkitIndexingModule.java
@@ -3,34 +3,39 @@
  */
 package se.repos.testing.cmstest;
 
-import org.tmatesoft.svn.core.wc.admin.SVNLookClient;
-
 import se.simonsoft.cms.backend.svnkit.CmsRepositorySvn;
-import se.simonsoft.cms.backend.svnkit.svnlook.CmsChangesetReaderSvnkitLookRepo;
-import se.simonsoft.cms.backend.svnkit.svnlook.CmsContentsReaderSvnkitLookRepo;
-import se.simonsoft.cms.backend.svnkit.svnlook.CmsRepositoryLookupSvnkitLook;
-import se.simonsoft.cms.backend.svnkit.svnlook.CommitRevisionCache;
-import se.simonsoft.cms.backend.svnkit.svnlook.CommitRevisionCacheDefault;
-import se.simonsoft.cms.backend.svnkit.svnlook.SvnlookClientProviderStateless;
+import se.simonsoft.cms.backend.svnkit.info.CmsRepositoryLookupSvnkit;
+import se.simonsoft.cms.backend.svnkit.info.change.CmsChangesetReaderSvnkit;
+import se.simonsoft.cms.backend.svnkit.info.change.CmsContentsReaderSvnkit;
+import se.simonsoft.cms.backend.svnkit.info.change.CommitRevisionCache;
+import se.simonsoft.cms.backend.svnkit.info.change.CommitRevisionCacheRepo;
 import se.simonsoft.cms.item.CmsRepository;
 import se.simonsoft.cms.item.info.CmsRepositoryLookup;
 import se.simonsoft.cms.item.inspection.CmsChangesetReader;
 import se.simonsoft.cms.item.inspection.CmsContentsReader;
 import se.simonsoft.cms.testing.svn.CmsTestRepository;
 
+import javax.inject.Provider;
+
+import org.tmatesoft.svn.core.io.SVNRepository;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 
 class SingleRepoSvnkitIndexingModule extends AbstractModule {
 
-	private CmsRepositorySvn repository;
+	private final CmsRepositorySvn repository;
+	//private final Provider<SVNRepository> svnkitProvider;
 
+	/*
 	public SingleRepoSvnkitIndexingModule(CmsTestRepository repository) {
 		this(CmsRepositorySvn.fromTesting(repository));
 	}
+	*/
 
-	public SingleRepoSvnkitIndexingModule(CmsRepositorySvn repository) {
+	public SingleRepoSvnkitIndexingModule(CmsRepositorySvn repository/*, Provider<SVNRepository> svnkitProvider*/) {
 		this.repository = repository;
+		//this.svnkitProvider = svnkitProvider;
 	}	
 	
 	@Override
@@ -38,12 +43,14 @@ class SingleRepoSvnkitIndexingModule extends AbstractModule {
 		bind(CmsRepository.class).toInstance(repository);
 		bind(CmsRepositorySvn.class).toInstance(repository);
 		
-		bind(SVNLookClient.class).toProvider(SvnlookClientProviderStateless.class);
-		bind(CmsChangesetReader.class).to(CmsChangesetReaderSvnkitLookRepo.class);
-		bind(CommitRevisionCache.class).to(CommitRevisionCacheDefault.class);
-		bind(CmsContentsReader.class).to(CmsContentsReaderSvnkitLookRepo.class);
-		bind(CmsRepositoryLookup.class).annotatedWith(Names.named("inspection")).to(CmsRepositoryLookupSvnkitLook.class); // deprecated, should not be in user-level context at all
-		bind(CmsRepositoryLookup.class).to(CmsRepositoryLookupSvnkitLook.class);
+		// TODO: Need to inject SVNRepository now when no longer injecting Svnlook provider?
+		//bind(SVNRepository.class).toInstance(svnkitProvider.get());
+		
+		bind(CmsChangesetReader.class).to(CmsChangesetReaderSvnkit.class);
+		bind(CommitRevisionCache.class).to(CommitRevisionCacheRepo.class);
+		bind(CmsContentsReader.class).to(CmsContentsReaderSvnkit.class);
+		//bind(CmsRepositoryLookup.class).annotatedWith(Names.named("inspection")).to(CmsRepositoryLookupSvnkitLook.class); // deprecated, should not be in user-level context at all
+		bind(CmsRepositoryLookup.class).to(CmsRepositoryLookupSvnkit.class);
 	}
 
 }
